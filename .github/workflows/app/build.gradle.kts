4df1533762f2dbad795fa6f09004164bf5b8dc64
randaladams <randy@adamselite.com>
2026-09-23 10:39:42 -0500

    Update build.gradle.kts

--- .github/workflows/app/build.gradle.kts
+++ .github/workflows/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.example.graceland"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
+        versionCode = 5
         versionName = "1.0"
     }
 
